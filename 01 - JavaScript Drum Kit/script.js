bf100751b7758edead0dc0bd7d2fbfc9be5b50af
andre <123985988+derre3@users.noreply.github.com>
2023-04-05 12:36:23 -0300

    add playSound()

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,1 +1,12 @@
+ function playSound(e) {
+    const audio = document.querySelector(`audio[data-key=${e.code}]`)
+    const key = document.querySelector(`.key[data-key=${e.code}]`)
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play()
+    key.classList.add('playing');
+  }
  
+
+  const keys = document.querySelectorAll('.key');
+  window.addEventListener('keydown', playSound);
